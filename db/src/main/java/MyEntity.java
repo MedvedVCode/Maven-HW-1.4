5ea6f38649c854e1374708f48d55ffde6df80dcb
Denis Fedorov <ssboshen@yandex.ru>
2022-07-29 11:14:48 +0300

    Maven homework files

---
+++ db/src/main/java/MyEntity.java
@@ -0,0 +1,35 @@
+import java.util.UUID;
+
+public class MyEntity {
+    private UUID id;
+    private String name;
+
+
+    public MyEntity(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("MyEntity{")
+                .append("id=").append(id).append(", name='")
+                .append(name).append('\'')
+                .append("}").toString();
+    }
+}
